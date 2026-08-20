ba8d167f8090288b5328b9567daf68b34b2592d9
Ashish singh <144147604+mrashishsingh@users.noreply.github.com>
2026-08-20 22:41:05 +0530

    Time: 1 ms (98.08%) | Memory: 46.5 MB (90.87%) - LeetSync

---
+++ 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] resultArray(int[] nums) {
+        int n = nums.length;
+        int[] a = new int[n];
+        int[] b = new int[n];
+        int x = 0, y = 0;
+
+        a[x++] = nums[0];
+        b[y++] = nums[1];
+
+        for (int i = 2; i < n; i++) {
+            if (a[x - 1] > b[y - 1])
+                a[x++] = nums[i];
+            else
+                b[y++] = nums[i];
+        }
+
+        int[] r = new int[n];
+        int k = 0;
+
+        for (int i = 0; i < x; i++)
+            r[k++] = a[i];
+
+        for (int i = 0; i < y; i++)
+            r[k++] = b[i];
+
+        return r;
+    }
+}
